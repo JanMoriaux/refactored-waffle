394df0c048cbf8dff6518f2a1fb2c3a385fc65d3
Jan Moriaux <jan.moriaux@aclagro.be>
2019-03-29 11:27:56 +0100

    MVC completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
+const errorController = require('./controllers/error');
 
 const path = require('path');
 
@@ -31,12 +32,9 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', '404'));
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
