c67a6a5e1bac1d047935ae0918bc61adebd14f1f
Jan Moriaux <jan.moriaux@aclagro.be>
2019-03-29 09:40:21 +0100

    ejs completed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+//const expressHbs = require('express-handlebars');
 
 const app = express();
 
@@ -14,15 +14,18 @@ const app = express();
 //app.set('view engine', 'pug');
 
 //we need to register handlebars as an engine first
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs'
+//   })
+// );
+// app.set('view engine', 'hbs');
+
+//ejs autoregisters with express
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
